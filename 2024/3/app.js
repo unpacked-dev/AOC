9c37b617c1ca7527a16d34f0b13fb835bd630fe3
unpacked <contact@unpacked.dev>
2024-12-03 11:05:48 +0100

    2024 Day 3 Part 1

---
+++ 2024/3/app.js
@@ -0,0 +1,18 @@
+console.time();
+
+import { import_puzzle_input } from "../../helper_functions.js";
+const PUZZLE_INPUT = import_puzzle_input();
+let result = 0;
+
+const reg_exp = /mul\(([0-9]{1,3}),([0-9]{1,3})\)/g;
+const muls = PUZZLE_INPUT.match(reg_exp);
+
+for(let mul of muls){
+    const digits_exp = /([0-9]{1,3})/g;
+    const digits = mul.match(digits_exp).map(Number);
+    const product = digits[0] * digits[1];
+    result += product;
+}
+
+console.log(result);
+console.timeEnd();
